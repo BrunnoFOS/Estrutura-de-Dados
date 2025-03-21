9f62ccad3b8832429f15e2781c38fe60b94a57ba
Brunno <brunnofos@outlook.com>
2025-03-20 21:37:15 -0300

    Resolução - Exercício 1

---
+++ FUNCOES_E_PASSAGENS_POR_REFERENCIA.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void trocar (int *valor, int *valor2){
+    int temp = *valor;
+    *valor = *valor2;
+    *valor2 = temp;
+}
+
+int main (){
+    int b = 1, n = 2;
+    
+    printf("Valor original de b = %d \n", b);
+    printf("Valor original de n = %d \n", n);
+    
+    trocar(&b, &n);
+    
+    printf("Valor de b após a troca = %d \n", b);
+    printf("Valor de n após a troca = %d \n", n);
+    
+    return 0;
+}
+
